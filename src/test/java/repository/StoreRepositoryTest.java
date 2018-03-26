af3e2fd7f2ddf01897bd33a2167ad3622b7b794e
alexandraneamtu <alexandraneamtu97@gmail.com>
2018-03-26 22:06:00 +0300

    added few more black box testing

--- src/test/java/repository/StoreRepositoryTest.java
+++ src/test/java/repository/StoreRepositoryTest.java
@@ -3,6 +3,8 @@ package repository;
 import com.sun.jmx.mbeanserver.Repository;
 import junit.framework.TestCase;
 import model.Product;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.IOException;
 
@@ -10,6 +12,7 @@ public class StoreRepositoryTest extends TestCase {
 
     StoreRepository repository;
 
+    @Before
     public void setUp() throws Exception {
         super.setUp();
         repository = new StoreRepository();
@@ -22,9 +25,26 @@ public class StoreRepositoryTest extends TestCase {
     }
 
     public void testAddNewProduct() throws IOException {
-        assertTrue(repository.getAllProducts().size()==0);
-        repository.addNewProduct(new Product(3,"ciocolata","aliment",4));
-        assertTrue(repository.getAllProducts().size()==1);
+        assertEquals(0,repository.getAllProducts().size());
+        repository.addNewProduct(new Product(2,"ciocolata","aliment",4));
+        assertEquals(1,repository.getAllProducts().size());
+    }
+
+    @Test
+    public void testAddProductCase1() throws IOException {
+        assertEquals(0,repository.getAllProducts().size());
+        repository.addNewProduct(new Product(-1,"ciocolata","aliment",5));
+        assertEquals(0,repository.getAllProducts().size());
+
+    }
+
+
+    @Test
+    public void testAddTestCase2() throws IOException {
+        assertEquals(0,repository.getAllProducts().size());
+        repository.addNewProduct(new Product(Integer.MAX_VALUE +1,"ciocolata","aliment",5));
+        assertEquals(0,repository.getAllProducts().size());
+
     }
 
     public void testGetProductsCategory() {
